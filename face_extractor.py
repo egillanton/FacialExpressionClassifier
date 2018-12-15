486f0e8c61349e25f48f5d9a783f76e57041f601
egillanton <egillanton@live.com>
2018-12-15 13:47:28 +0000

    Face Extracted Images Added

--- face_extractor.py
+++ face_extractor.py
@@ -3,6 +3,13 @@ import sys
 import cv2
 import matplotlib.pyplot as plt
 
+
+def rotate(image, deg):
+    rows, cols, _ = image.shape
+    M = cv2.getRotationMatrix2D((cols/2, rows/2), -deg, 1)
+    dst = cv2.warpAffine(image, M, (cols, rows))
+    return dst
+
 image_size = 128
 face_cascade = cv2.CascadeClassifier('./utils/haarcascade_frontalface_default.xml')
 
@@ -31,8 +38,25 @@ elif len(faces) >= 1:
 
     optFace = image[y:y+h, x:x+w]
 
-face_image = cv2.resize(optFace, (image_size, image_size))
+img = cv2.resize(optFace, (image_size, image_size))
+flipped_image = cv2.flip(img, 1)
+# Clockwise
+clockwise_rot5_image = rotate(img, 5)
+# Antin Clockwise
+anticlockwise_rot5_image = rotate(img, -5)
 
 plt.title("Detected Face")
-plt.imshow(cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB))
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.show()
+
+plt.title("Horizontal Flipped")
+plt.imshow(cv2.cvtColor(flipped_image, cv2.COLOR_BGR2RGB))
+plt.show()
+
+plt.title("Clockwise Rotated 5° Degrees")
+plt.imshow(cv2.cvtColor(clockwise_rot5_image, cv2.COLOR_BGR2RGB))
+plt.show()
+
+plt.title("Anticlockwise Rotated 5° Degrees")
+plt.imshow(cv2.cvtColor(anticlockwise_rot5_image, cv2.COLOR_BGR2RGB))
 plt.show()
